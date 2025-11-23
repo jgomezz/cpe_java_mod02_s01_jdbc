1d0bd927a730d8b3b429147d8b7596d228c58cac
jgomezz <jgomezz@gmail.com>
2025-11-23 12:55:02 -0500

    update

---
+++ src/main/java/pe/edu/tecsup/db/_4_EliminarRegistrosJDBC.java
@@ -0,0 +1,50 @@
+package pe.edu.tecsup.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class _4_EliminarRegistrosJDBC {
+
+    public static void main(String[] args) {
+
+        final String URL = "jdbc:mariadb://localhost/almacen";
+        final String USER = "root";
+        final String PASSWORD = "root";
+
+        try {
+            // Cargar el driver
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            // Conectarme a la Base de datos
+            Connection con = DriverManager.getConnection(URL,USER,PASSWORD);
+
+            // Preparar la sentencia SQL
+            String sql= """
+                DELETE FROM categorias WHERE id = ?
+            """;
+            PreparedStatement stmt= con.prepareStatement(sql);
+
+            // Configura el PK del registro a eliminar
+            int id = 4; // ID a borrar
+            stmt.setInt(1, id);
+
+            // Ejecutar el borrado
+            int estado = stmt.executeUpdate();
+            if (estado != 1)
+                throw new SQLException("No se pudo eliminar");
+
+            System.out.printf("Se borro la categoria con id = %d \n", id);
+
+            // Cerrar conexiones
+            stmt.close();
+            con.close();
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
