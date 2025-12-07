eb566ec47205023b9c5c2779fe90cbe0ff5121cf
jgomezz <jgomezz@gmail.com>
2025-12-07 09:10:06 -0500

    implementado clase ConexionDB

--- src/main/java/pe/edu/tecsup/db/v2/dao/CategoriaDAOImpl.java
+++ src/main/java/pe/edu/tecsup/db/v2/dao/CategoriaDAOImpl.java
@@ -1,14 +1,47 @@
 package pe.edu.tecsup.db.v2.dao;
 
 import pe.edu.tecsup.db.v2.entidad.Categoria;
+import pe.edu.tecsup.db.v2.utils.ConexionDB;
 
+import java.sql.*;
 import java.util.List;
 
 public class CategoriaDAOImpl implements CategoriaDAO {
 
     @Override
     public List<Categoria> obtenerCategorias() {
+
+        try {
+
+            // Conectarme a la Base de datos
+            Connection con = ConexionDB.obtenerConexion();
+
+            // Preparar la sentencia
+            String sql= "SELECT * FROM categorias";
+            PreparedStatement stmt= con.prepareStatement(sql);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()) {
+                int id= rs.getInt("id");
+                String nombre = rs.getString("nombre");
+                System.out.printf("id = %d , nombre=%s \n", id,nombre);
+            }
+
+            rs.close();
+            stmt.close();
+            con.close();
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         return List.of();
+
     }
 
 }
--- src/main/java/pe/edu/tecsup/db/v2/utils/ConexionDB.java
+++ src/main/java/pe/edu/tecsup/db/v2/utils/ConexionDB.java
@@ -1,4 +1,22 @@
 package pe.edu.tecsup.db.v2.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConexionDB {
+
+    final static String URL = "jdbc:mariadb://localhost/almacen";
+    final static String USER = "root";
+    final static String PASSWORD = "root";
+
+    public static Connection obtenerConexion() throws Exception {
+
+        // Cargar el driver
+        Class.forName("org.mariadb.jdbc.Driver");
+
+        // Conectarme a la Base de datos
+        Connection con = DriverManager.getConnection(URL,USER,PASSWORD);
+
+        return con;
+    }
 }
