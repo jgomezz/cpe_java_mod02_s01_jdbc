795d726b7701aa4c5f7527c1c6221cd56adbeb87
jgomezz <jgomezz@gmail.com>
2025-12-07 09:22:28 -0500

    actualizado

--- src/main/java/pe/edu/tecsup/db/v2/dao/CategoriaDAOImpl.java
+++ src/main/java/pe/edu/tecsup/db/v2/dao/CategoriaDAOImpl.java
@@ -4,6 +4,7 @@ import pe.edu.tecsup.db.v2.entidad.Categoria;
 import pe.edu.tecsup.db.v2.utils.ConexionDB;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CategoriaDAOImpl implements CategoriaDAO {
@@ -11,21 +12,38 @@ public class CategoriaDAOImpl implements CategoriaDAO {
     @Override
     public List<Categoria> obtenerCategorias() {
 
+        List<Categoria> categorias = new ArrayList<Categoria>();
+
         try {
 
             // Conectarme a la Base de datos
             Connection con = ConexionDB.obtenerConexion();
 
             // Preparar la sentencia
-            String sql= "SELECT * FROM categorias";
+            String sql= """
+                            SELECT * FROM categorias
+                        """;
             PreparedStatement stmt= con.prepareStatement(sql);
 
             ResultSet rs = stmt.executeQuery();
 
             while(rs.next()) {
+
+                // Lee informacion de un registro de la tabla categorias
                 int id= rs.getInt("id");
                 String nombre = rs.getString("nombre");
-                System.out.printf("id = %d , nombre=%s \n", id,nombre);
+                String descripcion = rs.getString("descripcion");
+                String orden = rs.getString("orden");
+
+                // Crea un objeto Categoria
+                Categoria categoria = new Categoria();
+                categoria.setId(id);
+                categoria.setNombre(nombre);
+                categoria.setDescripcion(descripcion);
+                categoria.setOrden(orden);
+
+                // Agrega el objeto a la lista
+                categorias.add(categoria);
             }
 
             rs.close();
@@ -40,7 +58,7 @@ public class CategoriaDAOImpl implements CategoriaDAO {
             throw new RuntimeException(e);
         }
 
-        return List.of();
+        return categorias;
 
     }
 
